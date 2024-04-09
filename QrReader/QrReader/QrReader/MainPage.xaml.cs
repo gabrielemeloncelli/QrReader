841264db1163a8f0b8498944e8d0b5b60f930146
Gabriele Meloncelli <gabriele.meloncelli@bi-way.it>
2024-04-09 12:34:03 +0200

    Add the files to the VCS
    Prototype developed to make sure it can be used
    and deployed on Android 13 Tablet

---
+++ QrReader/QrReader/QrReader/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace QrReader
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            zxing.OnScanResult += (result) =>
+    Device.BeginInvokeOnMainThread(() =>
+    {
+        lblResult.Text = result.Text;
+    });
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            zxing.IsScanning = true;
+        }
+
+        protected override void OnDisappearing()
+        {
+            zxing.IsScanning = false;
+
+            base.OnDisappearing();
+        }
+    }
+}
